f1e3a57361fb0d0fd4ef19e88a9a103ace43e0f3
jvmaiaa <joaovictormaia482@gmail.com>
2025-07-14 04:06:32 -0300

    ✨[FEAT]: Cria classe para transformar um useCase em um Bean de forma Manual (seguindo padrões do Clean Arch).

---
+++ src/main/java/br/com/gerecia_estoque/modules/produto/main/config/ProdutoConfig.java
@@ -0,0 +1,22 @@
+package br.com.gerecia_estoque.modules.produto.main.config;
+
+import br.com.gerecia_estoque.modules.produto.application.gateways.ProdutoGateway;
+import br.com.gerecia_estoque.modules.produto.application.usecases.CreateProdutoCase;
+import br.com.gerecia_estoque.modules.produto.application.usecases.impl.CreateProdutoCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProdutoConfig {
+
+    /**
+     * Creates a bean for the CreateProdutoCase use case.
+     * 
+     * @param produtoGateway The gateway for produto operations
+     * @return An implementation of CreateProdutoCase
+     */
+    @Bean // Qual implementação o Spring deve usar quando injetar CreateProdutoCase? nesse caso: CreateProdutoCaseImpl
+    public CreateProdutoCase createProdutoCase(ProdutoGateway produtoGateway) {
+        return new CreateProdutoCaseImpl(produtoGateway);
+    }
+}
