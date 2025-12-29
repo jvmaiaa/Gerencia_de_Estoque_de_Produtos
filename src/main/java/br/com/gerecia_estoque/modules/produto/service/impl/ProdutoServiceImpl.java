c483ed3be994ff6eb88c138b703383d896ca07c6
jvmaiaa <joaovictormaia482@gmail.com>
2025-12-29 02:14:34 -0300

    ✨feat: Cria todos os métodos do service de produtos.

--- src/main/java/br/com/gerecia_estoque/modules/produto/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/gerecia_estoque/modules/produto/service/impl/ProdutoServiceImpl.java
@@ -6,10 +6,17 @@ import br.com.gerecia_estoque.modules.produto.service.ProdutoService;
 import br.com.gerecia_estoque.modules.produto.web.dtos.ProdutoRequestDTO;
 import br.com.gerecia_estoque.modules.produto.web.dtos.ProdutoResponseDTO;
 import br.com.gerecia_estoque.modules.produto.web.mapper.ProdutoMapper;
+import br.com.gerecia_estoque.shared.exceptions.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static br.com.gerecia_estoque.shared.exceptions.ExceptionMessages.PRODUTO_NAO_ENCONTRADO;
 
 @RequiredArgsConstructor
 @Service
@@ -18,10 +25,44 @@ public class ProdutoServiceImpl implements ProdutoService {
     private final ProdutoRepository produtoRepository;
     private final ProdutoMapper produtoMapper;
 
-    public ProdutoResponseDTO saveProduto(ProdutoRequestDTO produtoRequestDTO) {
+    public ProdutoResponseDTO save(ProdutoRequestDTO produtoRequestDTO) {
         ProdutoEntity produtoSaved = produtoMapper.requestToEntity(produtoRequestDTO);
         produtoSaved.setDataCadastro(LocalDateTime.now());
+
         produtoRepository.save(produtoSaved);
+
         return produtoMapper.entityToResponse(produtoSaved);
+    }
+
+    @Override
+    public Page<ProdutoResponseDTO> findAllPaginated(Pageable pageable) {
+        return produtoRepository.findAll(pageable).map(produtoMapper::entityToResponse);
+    }
+
+    @Override
+    public ProdutoResponseDTO findById(UUID uuid) {
+        ProdutoEntity produtoEntity = produtoRepository.findById(uuid)
+                .orElseThrow(() -> new EntityNotFoundException(PRODUTO_NAO_ENCONTRADO));
+        return produtoMapper.entityToResponse(produtoEntity);
+    }
+
+    @Override
+    @Transactional
+    public ProdutoResponseDTO update(UUID uuid, ProdutoRequestDTO produtoRequestDTO) {
+        ProdutoEntity produtoEntity = produtoRepository.findById(uuid)
+                .orElseThrow(() -> new EntityNotFoundException(PRODUTO_NAO_ENCONTRADO));
+
+        produtoMapper.updateEntityFromRequestDTO(produtoRequestDTO, produtoEntity);
+        produtoRepository.save(produtoEntity);
+
+        return produtoMapper.entityToResponse(produtoEntity);
+    }
+
+    @Override
+    public void delete(UUID uuid) {
+        ProdutoEntity produtoEntity = produtoRepository.findById(uuid)
+                .orElseThrow(() -> new EntityNotFoundException(PRODUTO_NAO_ENCONTRADO));
+
+        produtoRepository.deleteById(uuid);
     }
 }
