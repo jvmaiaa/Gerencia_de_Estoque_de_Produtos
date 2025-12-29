396acc58b10b966c19e2d0accf42564bebee0ecc
jvmaiaa <joaovictormaia482@gmail.com>
2025-12-29 02:14:04 -0300

    ✨feat: Adiciona método no mapper para atualização de campos no método PUT.

--- src/main/java/br/com/gerecia_estoque/modules/produto/web/mapper/ProdutoMapper.java
+++ src/main/java/br/com/gerecia_estoque/modules/produto/web/mapper/ProdutoMapper.java
@@ -3,8 +3,11 @@ package br.com.gerecia_estoque.modules.produto.web.mapper;
 import br.com.gerecia_estoque.modules.produto.domain.entity.ProdutoEntity;
 import br.com.gerecia_estoque.modules.produto.web.dtos.ProdutoRequestDTO;
 import br.com.gerecia_estoque.modules.produto.web.dtos.ProdutoResponseDTO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
@@ -16,11 +19,15 @@ public interface ProdutoMapper {
     @Mapping(target = "dataUltimaAtualizacao", ignore = true)
     ProdutoEntity requestToEntity(ProdutoRequestDTO produtoRequestDTO);
 
+    ProdutoResponseDTO entityToResponse(ProdutoEntity produtoEntity);
+
 //    @Mapping(target = "id", ignore = true)
 //    @Mapping(target = "dataCadastro", ignore = true)
 //    @Mapping(target = "dataAtualizacao", ignore = true)
 //    ProdutoEntity requestDTOToDomain(ProdutoRequestDTO produtoRequestDTO);
 
-
-    ProdutoResponseDTO entityToResponse(ProdutoEntity produtoEntity);
+    // informa ao mapper para ignorar campos nulos no DTO de requisição
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromRequestDTO(ProdutoRequestDTO produtoRequestDTO,
+                                    @MappingTarget ProdutoEntity produtoEntity);
 }
